211dd6f95137414e8aae7e7f49fff6a1fc31dcb0
andrija-ei <andrija.avramovic@engineeringinsights.com>
2025-04-18 13:49:39 +0200

    Auth update

--- utils/checkForAuth.ts
+++ utils/checkForAuth.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 
-export async function checkForAuth() {
+export function checkForAuth() {
   if (!fs.existsSync("auth.json")) {
     throw new Error(
       "Missing auth.json. Please run `save-auth.ts` locally and commit the file."
